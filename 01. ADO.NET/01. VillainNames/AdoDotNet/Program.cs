b0d8e5193f0f5ea1702ee39d4fc0ddddab791c76
IvanAbadzhiev01 <ivan087611@gmail.com>
2024-12-11 09:27:12 +0200

    Create Program.cs

---
+++ 01. ADO.NET/01. VillainNames/AdoDotNet/Program.cs
@@ -0,0 +1,19 @@
+﻿using AdoDotNet;
+using System.Data.SqlClient;
+
+const string __connectionString = "Server=LAPTOP-9CAVEGVR\\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+
+using SqlConnection dbCon = new SqlConnection(__connectionString);
+await dbCon.OpenAsync();
+
+
+using SqlCommand getCom = new SqlCommand(SqlQueries.__query, dbCon);
+
+using SqlDataReader sqlDr = getCom.ExecuteReader();
+
+while(await sqlDr.ReadAsync())
+{
+    Console.WriteLine($"{sqlDr["Name"]} - {sqlDr["TotalMinions"]}");
+}
+
+

44d4c5b08d3febcfce03bd2c78bf41628a54a9a0
IvanAbadzhiev01 <ivan087611@gmail.com>
2024-12-11 09:27:19 +0200

    Create SqlQueries.cs

---
+++ 01. ADO.NET/01. VillainNames/AdoDotNet/SqlQueries.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoDotNet
+{
+    public class SqlQueries
+    {
+
+      public  const string __query = @"
+    SELECT 
+	[Name] 
+    ,COUNT(*) AS TotalMinions
+    FROM Villains AS v
+    JOIN MinionsVillains AS mv ON mv.VillainId = v.Id
+    GROUP BY [Name]
+    HAVING COUNT(*) > 3
+    ORDER BY TotalMinions";
+
+    }
+}

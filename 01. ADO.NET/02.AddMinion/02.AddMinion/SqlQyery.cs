cfd8acee0f33285f5940a42f651688fb982fc3f7
IvanAbadzhiev01 <ivan087611@gmail.com>
2024-12-16 16:04:23 +0200

    Create SqlQyery.cs

---
+++ 01. ADO.NET/02.AddMinion/02.AddMinion/SqlQyery.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.AddMinion
+{
+    public class SqlQyery
+    {
+        public const string GetTownByName = @"SELECT Id FROM Towns WHERE Name = @townName";
+
+        public const string GetVilainByName = @"SELECT Id FROM Villains WHERE Name = @Name";
+
+        
+        
+        
+        
+
+
+
+        public const string AddVilain = @"INSERT INTO Villains (Name, EvilnessFactorId) OUTPUT inserted.Id  VALUES (@villainName, 4)";
+
+
+        public const string AddTown = @"INSERT INTO Towns (Name), OUTPUT inserted.Id VALUES (@townName)";
+
+
+        public const string AddMinion = @"INSERT INTO Minions (Name, Age, TownId) OUTPUT inserted.Id VALUES (@name, @age, @townId)";
+
+
+        public const string insertMv = @"INSERT INTO MinionsVillains (MinionId, VillainId) VALUES (@minionId, @villainId)";
+
+
+
+    }
+}

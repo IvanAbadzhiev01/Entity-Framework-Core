b3a8db82130431981c7c7a598fd060cf40a426bd
IvanAbadzhiev01 <ivan087611@gmail.com>
2025-01-03 09:51:22 +0200

    Create StartUp.cs

---
+++ 03. Entity Framework Introduction/03EmployeesWithSalaryOver50000/SoftUni/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using SoftUni.Data;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+
+        static void Main(string[] args)
+        {
+
+            SoftUniContext context = new SoftUniContext();
+
+            Console.WriteLine(GetEmployeesWithSalaryOver50000(context)); ;
+
+
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context)
+        {
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+
+
+                }).Where(e => e.Salary > 50000).OrderBy(e => e.FirstName).ToList();
+
+            string result = string.Join(Environment.NewLine, employees
+                .Select(e => $"{e.FirstName} - {e.Salary:f2}"));
+            return result;
+        }
+
+
+
+
+    }
+}
